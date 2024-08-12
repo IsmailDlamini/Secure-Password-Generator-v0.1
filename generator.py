64caa8551133ab8c831a36a47035d8611f0bb0ff
Ismail <68245583+IsmailDlamini@users.noreply.github.com>
2024-08-12 12:57:35 +0200

    changed and improved the code for generating a passoword

--- generator.py
+++ generator.py
@@ -1,11 +1,8 @@
 
+#Code written by Ismail Dlamini
+#last updated 2024/08/12
+
 import random
-
-#pass1 = ""
-
-#s1 = "off"
-#s2 = "on"
-#s3 = "on"
 
 def generate_password_file(password, pass_len, u_let, u_num, u_spec):
 
@@ -13,109 +10,24 @@ def generate_password_file(password, pass_len, u_let, u_num, u_spec):
     numbers = "123456789"
     characters = "!@#$%&"
 
-    choices = [letters,numbers,characters]
-    store = []
+    choices = []
+    password_storage = []
+   
+    if u_let == "on" : choices.append(letters) # add letters to the choices if selected
+    if u_spec == "on" : choices.append(characters) # add special letters to the choices if selected
+    if u_num == "off" : choices.append(numbers) # add numbers to the choices if selected
 
-    let = list(letters)
-    nums = list(numbers)
-    chars = list(characters)
+    password_storage = []
 
-    
-    if u_let == "on" and u_num == "on" and u_spec == "on":
-        for i in range(pass_len):
-            decider = random.randint(0,2)
-            if decider == 0:
-                store.append(let[random.randint(0, 51)])
+    for i in range(pass_len):
+            character_set_decider = random.randint(0, len(choices) - 1)
+            character_decider = random.randint(0, len(choices[character_set_decider]) - 1)
+            password_storage.append(choices[character_set_decider][character_decider])
 
-            if decider == 1:
-                store.append(nums[random.randint(0, 8)])
-
-            if decider == 2:
-                store.append(chars[random.randint(0, len(chars) - 1)])
+    return "".join(password_storage)
 
 
-        new_pass = ''.join(store)
-        final_pass = password
-        return(new_pass)
-
-    elif  u_let == "on" and u_num == "on" and u_spec == "off":
-
-        for i in range(pass_len):
-
-            decider = random.randint(0,1)
-            if decider == 0:
-                store.append(let[random.randint(0, 51)])
-
-            if decider == 1:
-                store.append(nums[random.randint(0, 8)])
-
-        new_pass = ''.join(store)
-        final_pass = password
-        return(new_pass)
-
-    elif u_let == "on" and u_spec == "on" and u_num == "off":
-
-        for i in range(pass_len):
-
-            decider = random.randint(0,1)
-            if decider == 0:
-                store.append(let[random.randint(0, 51)])
-
-            if decider == 1:
-                store.append(chars[random.randint(0, len(chars) - 1)])
-
-        new_pass = ''.join(store)
-        final_pass = password
-        return(new_pass)
-
-    
-    elif u_num == "on" and u_spec == "on" and u_let == "off":
-        for i in range(pass_len):
-            decider = random.randint(0,1)
-            if decider == 0:
-               store.append(nums[random.randint(0, 8)]) 
-
-            if decider == 1:
-               store.append(chars[random.randint(0, len(chars) - 1)])
-
-        new_pass = ''.join(store)
-        final_pass = password
-        return(new_pass)
-
-
-    elif u_let == "on" and u_num == "off" and u_spec == "off":
-        for i in range(pass_len):
-            count = random.randint(0, 2)
-            store.append(let[random.randint(0, 51)])
-
-        new_pass = ''.join(store)
-        final_pass = password
-        return(new_pass)
-
-
-    elif u_let == "off" and u_num == "on" and u_spec == "off":
-        for i in range(pass_len):
-            count = random.randint(0, 2)
-            store.append(nums[random.randint(0, 8)])
-
-        new_pass = ''.join(store)
-        final_pass = password
-        return(new_pass)
-
-
-    elif u_let == "off" and u_num == "off" and u_spec == "on":
-        for i in range(pass_len):
-            count = random.randint(0, 2)
-            store.append(chars[random.randint(0, len(chars) - 1)])
-
-        new_pass = ''.join(store)
-        final_pass = password
-        return(new_pass)
-
-
-    
-
-#print(generate_password_file(pass1, 15, s1, s2, s3))
+    #copyright @ reserved Ismail Dlamini 2024
     
 
 
